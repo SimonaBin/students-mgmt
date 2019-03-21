c329d07c67dad02da1e14acd42f3034ab1697c06
simona.bineata <simona.bineata@BUK30LAP4001297.client.ro.r4.madm.net>
2019-03-21 12:18:48 +0200

    initial commit

---
+++ src/main/java/net/metrosystems/competition/studentsmgmt/StudentsMgmtApplication.java
@@ -0,0 +1,13 @@
+package net.metrosystems.competition.studentsmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsMgmtApplication.class, args);
+	}
+
+}
